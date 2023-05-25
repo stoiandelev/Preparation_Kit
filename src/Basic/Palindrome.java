8e722e30912a30f7aa019759fbfa58fb2ae10f99
stoiandelev <stoian.delev@yahoo.com>
2023-05-25 17:51:59 +0300

    Palindrome implementation

---
+++ src/Basic/Palindrome.java
@@ -0,0 +1,30 @@
+package Basic;
+
+import java.util.Scanner;
+
+public class Palindrome {
+
+    /**
+     * Palindrome is word that when we read from begging to end
+     * and end to begin is the same
+     */
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String word = scanner.nextLine().toLowerCase();
+
+        StringBuilder reversedString = new StringBuilder();
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversedString.append(word.charAt(i));
+        }
+
+        if (word.equals(reversedString.toString())){
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+
+}
