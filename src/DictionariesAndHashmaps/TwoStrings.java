c622a123e02571a5f85dee11d74d957d060572e6
stoiandelev <stoian.delev@yahoo.com>
2023-05-27 16:19:11 +0300

    Two string - determine if they share a common substring.

---
+++ src/DictionariesAndHashmaps/TwoStrings.java
@@ -0,0 +1,59 @@
+package DictionariesAndHashmaps;
+
+import java.io.*;
+import java.util.HashSet;
+import java.util.stream.IntStream;
+
+
+//2
+//hello
+//world
+//hi
+//world
+
+//Given two strings, determine if they share a common substring.
+// A substring may be as small as one character.
+
+public class TwoStrings {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int numberOfTestCase = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, numberOfTestCase).forEach(qItr -> {
+            try {
+                String string1 = bufferedReader.readLine();
+                String string2 = bufferedReader.readLine();
+                String result = TwoStrings.twoStrings(string1, string2);
+                System.out.println(result);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+    }
+
+
+    public static String twoStrings(String string1, String string2) {
+
+        HashSet<Character> rootHashSet = new HashSet<>();
+        HashSet<Character> hashSet1 = new HashSet<>();
+        HashSet<Character> hashSet2 = new HashSet<>();
+
+        string1.chars().forEach(c -> {
+            rootHashSet.add((char) c);
+            hashSet1.add((char) c);
+        });
+        string2.chars().forEach(c -> {
+            rootHashSet.add((char) c);
+            hashSet2.add((char) c);
+        });
+
+        if (hashSet1.size() + hashSet2.size() != rootHashSet.size()) {
+            return "YES";
+        }
+        return "NO";
+
+    }
+}
